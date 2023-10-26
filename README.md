273dfa87ac7e6895edb4edf29df2c09bf8a9635f
CodeSandbox <support@codesandbox.io>
2023-10-26 00:14:21 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-# Vite + Solid
+# Supabase Sockets
 
-This is a [Vite](https://vitejs.dev) project together with Solid.
+To repeat the issue:
 
-[![Edit in CodeSandbox](https://assets.codesandbox.io/github/button-edit-lime.svg)](https://codesandbox.io/p/github/codesandbox/codesandbox-template-vite-solid/main)
-
-[Configuration](https://codesandbox.io/docs/projects/learn/setting-up/tasks) has been added to optimize it for [CodeSandbox](https://codesandbox.io).
-
-## Resources
-
-- [CodeSandbox — Docs](https://docs.codesandbox.io)
-- [CodeSandbox — Discord](https://discord.gg/Ggarp3pX5H)
-- [Vite — GitHub](https://github.com/vitejs/vite)
-- [Vite — Docs](https://vitejs.dev/guide/)
+1. Open this site on your mobile device browser
+2. Notice it reports that it's connected to the supabase Sockets
+3. Put the browser in the background (don't hard close)
+4. Wait about 10 min
+5. Open the browser again
+6. Notice that it CLOSED and TIMED_OUT and refuses to reconnect.
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import './App.css'
+import { createEffect, createSignal } from "solid-js";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://slfiurejebbbktmimjfe.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsZml1cmVqZWJiYmt0bWltamZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgyNzk0NTIsImV4cCI6MjAxMzg1NTQ1Mn0.kyx1fZkIbSveiAjIQrNeoavztiL0n86br9soU5R0o0U";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const startConnection = async ({ onEvent, onConnectionChange }) => {
+  try {
+    await supabase.channel("time").unsubscribe();
+    await supabase
+      .channel("time")
+      .on(
+        "postgres_changes",
+        { event: "UPDATE", schema: "public", table: "countries" },
+        (payload) => {
+          onEvent(payload);
+        },
+      )
+      .subscribe((e) => {
+        onConnectionChange(e);
+      });
+  } catch (err) {
+    onConnectionChange(`Exception ${err.message}`);
+  }
+};
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [log, setLog] = createSignal("Started");
+  const [isVisible, setIsVisible] = createSignal(!document.hidden);
+
+  // Listen for the tab/window to become visible again
+  // With that we try to reconnect:
+  document.addEventListener("visibilitychange", () => {
+    setLog((l) => l + `\n visible: ${!document.hidden}`);
+    setIsVisible(!document.hidden);
+  });
+
+  createEffect(() => {
+    if (isVisible()) {
+      setLog((l) => l + `\n Is vis changed`);
+      startConnection({
+        onEvent: (e) => setLog((l) => l + `\n ${e}`),
+        onConnectionChange: (e) => setLog((l) => l + `\n ${e}`),
+      });
+    }
+  });
 
   return (
-    <div class="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://www.solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
+    <div>
+      <p>Test</p>
+      <pre>{log()}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
